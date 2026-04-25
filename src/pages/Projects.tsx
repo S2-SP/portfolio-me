91d30c09ede98bfc6a98012dcf4d0444457dd41b
S2-SP <65578209+S2-SP@users.noreply.github.com>
2026-04-25 11:09:26 +1000

    Update links

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,21 +9,21 @@ const projects = [
     description: "A simple clock built using HTML, CSS, and JavaScript to demonstrate animation techniques.",
     videoUrl: "./clock.mp4", 
     githubUrl: "https://github.com/S2-SP/Clock-Project",
-    link:"https://clock-project-flame.vercel.app/"
+    link:"https://digital-analog-clock-sp.netlify.app/"
   },
   {
     title: "Weather",
     description: "A simple weather app built using HTML, CSS, and JavaScript to get the weather details in different cities.",
     videoUrl: "./weather.mp4",
     githubUrl: "https://github.com/S2-SP/weather-app", 
-    link:"https://weather-app-steel-kappa-74.vercel.app/"
+    link:"https://weather-details-sp.netlify.app/"
   },
   {
     title:"Gallery of words",
     description:"Gallery of Words is a full-stack poetry publishing platform where an admin can write, publish, edit, and delete poems, and the public can browse and read them. It is a single-page application built with a modern React frontend and a managed cloud database backend.",
     videoUrl:"./gallery-of-words.mov",
     githubUrl:"https://github.com/S2-SP/poetry",
-    link:"https://poetry-o1elmml35-s2-sps-projects.vercel.app/"
+    link:"https://sefrina-offline-poetry.netlify.app/"
   },
   {
     title:"Expense Tracker",
